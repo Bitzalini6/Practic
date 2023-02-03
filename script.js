73e6c79f09989a6033b60175b433e6c0b82029b8
Gabriel Giurca <96671507+Bitzalini6@users.noreply.github.com>
2023-02-04 00:30:20 +0200

    todolist2

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const addForm = document.querySelector('.add');
 
+addForm.addEventListener('submit', e =>{
+    e.preventDefault();
+const todo = addForm.add.value.trim();
+console.log(todo)
+
+});
 
 
 
